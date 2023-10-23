6e3334e3f9eb3dd66ed37cf4bbb30de5bfb5ff21
ardevi44 <arbey9621@gmail.com>
2023-10-22 19:31:10 -0600

    Overloadig constructors
    And call them from other constructor.

--- OOP/Practice/Overloading.cs
+++ OOP/Practice/Overloading.cs
@@ -34,6 +34,18 @@ namespace OOP.Practice
       _date = date;
     }
 
+    // Calling a constructor from another.
+    public MedicalAppointment(string patientName) :
+    this(patientName, 7)
+    {
+    }
+
+    public MedicalAppointment(string patientName, int daysFromNow)
+    {
+      _patientName = patientName;
+      _date = DateTime.Now.AddDays(daysFromNow);
+    }
+
     public void Reschedule(DateTime date)
     {
       _date = date;
